ad8c02dacdc128f95a6186ad269cb9e13d7f59a1
Brunonbt <129996122+Brunonbt@users.noreply.github.com>
2023-06-26 18:29:58 -0300

    Archivos de la evidencia N°5
    Se suben archivos de evidencia n°5

---
+++ #menu_v2.py
@@ -0,0 +1,136 @@
+#menu vercion 2
+import conexion
+from datetime import datetime
+def mostrar_menu():
+    print("                         MENU          ")
+    print("¡Elija una opcion ingresando el numero correspondiente!")
+    print("1_ Dispositivos")
+    print("2_ Instalaciones")
+    print("3_ Salir")
+
+def mostrar_menu_dispositivos():
+    print("             MENU DISPOSITIVOS          ")
+    print("1_ Actualizar dispositivo")
+    print("2_ Insertar un nuevo dispositivo")
+
+def mostrar_menu_instalaciones():
+    print("             MENU INSTALACIONES          ")
+    print("1_ Actualizar instalación")
+    print("2_ Insertar una nueva instalación")
+
+def actualizar_dispositivo():
+    # Pedir datos del dispositivo a actualizar
+    id_dispositivo = int(input("Ingrese el ID del dispositivo a actualizar: "))
+    modelo_nuevo = str(input("Ingrese el nombre del nuevo modelo de dispositivo: ")) 
+    numero_serie_nuevo = str(input("Ingrese el numero de serie del nuevo dispositivo: "))
+    estado_nuevo = str(input("Ingrese el estado del dispositivo por ej: Operativo / No Operativo:  "))
+
+    # Crear una instancia del conector de base de datos
+    conexion_bd = conexion.Conexion(host="localhost", user="root", password="Talleres1999", database="proyecto_integrador_1")
+
+    # Conectar a la base de datos
+    conexion_bd.connect()
+
+    # Actualizar el dispositivo en la base de datos
+    conexion_bd.actualizar_dispositivo(modelo_nuevo, numero_serie_nuevo, estado_nuevo, id_dispositivo)
+
+    # Desconectar de la base de datos
+    conexion_bd.disconnect()
+
+    print("Dispositivo actualizado correctamente")
+    
+
+def ingresar_dispositivo():
+    # Pedir datos del nuevo dispositivo
+    modelo = str(input("Ingrese el modelo del nuevo dispositivo: "))
+    numero_serie = str(input("Ingrese el número de serie del nuevo dispositivo: "))
+    estado = str(input("Ingrese el estado del nuevo dispositivo: "))
+
+    # Crear una instancia del conector de base de datos
+    conexion_bd = conexion.Conexion(host="localhost", user="root", password="Talleres1999", database="proyecto_integrador_1")
+
+    # Conectar a la base de datos
+    conexion_bd.connect()
+
+    # Insertar el dispositivo en la base de datos
+    conexion_bd.insertar_dispositivo(modelo, numero_serie, estado)
+
+    # Desconectar de la base de datos
+    conexion_bd.disconnect()
+
+    print("Dispositivo insertado correctamente")
+    
+def ingresar_instalacion():
+    #pedir datos de la instalacion a ingresar
+    dispositivo_id = int (input("Ingrese el ID de la instalación: "))
+    direccion_instalacion =str (input("Ingrese la dirección de la instalación: "))
+    fecha_instalacion= int (input("Ingrese la nueva fecha de la instalación (AAAA-MM-DD): "))
+    coordenadas = int (input("Ingrese las nuevas coordenadas de la instalación: "))
+
+    # Crear una instancia del conector de base de datos
+    conexion_bd = conexion.Conexion(host="localhost", user="root", password="Talleres1999", database="proyecto_integrador_1")
+
+    # Conectar a la base de datos
+    conexion_bd.connect()
+
+    # Actualizar la instalación en la base de datos
+    conexion_bd.ingresar_instalacion(dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas)
+
+    # Desconectar de la base de datos
+    conexion_bd.disconnect()
+
+    print("Instalación actualizada correctamente")
+    
+def actualizar_instalacion():
+    # Pedir datos de la instalación a actualizar
+    dispositivo_id = int(input("Ingrese el ID de la instalación a actualizar: "))
+    direccion_instalacion = input("Ingrese la nueva dirección de la instalación: ")
+    fecha_instalacion= input("Ingrese la nueva fecha de la instalación (AAAA-MM-DD): ")
+    coordenadas = input("Ingrese las nuevas coordenadas de la instalación: ")
+
+    # Crear una instancia del conector de base de datos
+    conexion_bd = conexion.Conexion(host="localhost", user="root", password="Talleres1999", database="proyecto_integrador_1")
+
+    # Conectar a la base de datos
+    conexion_bd.connect()
+
+    # Actualizar la instalación en la base de datos
+    conexion_bd.actualizar_instalacion(dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas)
+
+    # Desconectar de la base de datos
+    conexion_bd.disconnect()
+
+    print("Instalación actualizada correctamente")
+    
+# Mostrar el menú y solicitar la opción al usuario
+mostrar_menu()
+
+opcion_menu =int (input("Ingrese la opción deseada: "))
+
+if opcion_menu == 1:
+    # Opción de dispositivos
+    mostrar_menu_dispositivos()
+    opcion_dispositivos = input("Ingrese la opción deseada: ")
+    if opcion_dispositivos == "1":
+        actualizar_dispositivo()
+    elif opcion_dispositivos == "2":
+        ingresar_dispositivo()
+    else:
+        print("Opción no válida")
+
+elif opcion_menu == 2:
+    # Opción de instalaciones
+    mostrar_menu_instalaciones()
+    opcion_instalaciones = input("Ingrese la opción deseada: ")
+    if opcion_instalaciones == "1":
+        actualizar_instalacion()
+    elif opcion_instalaciones == "2":
+        ingresar_instalacion()
+    else:
+        print("Opción no válida")
+
+elif opcion_menu == 3:
+    print("Hasta luego. ¡Gracias por utilizar el programa!")
+
+else:
+    print("Opción no válida")
---
+++ conexion_v2.py
@@ -0,0 +1,100 @@
+import mysql.connector
+from datetime import datetime
+
+fecha_str = "2023-06-22"
+fecha = datetime.strptime(fecha_str, "%Y-%m-%d")
+
+
+class Conexion:
+    def __init__(self, host, user, password, database):
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "Stratocaster333"
+        self.database = "Proyectointegradorv01"
+        self.connection = None
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        if self.connection.is_connected():
+            print("Conexión exitosa")
+
+    def disconnect(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("Desconexión")
+
+    def leer_dispositivos(self):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "SELECT * FROM dispositivos"
+            cursor.execute(query)
+            dispositivos = cursor.fetchall()
+            cursor.close()
+            return dispositivos
+        else:
+            print("No hay conexión a la base de datos")
+
+    def ingresar_dispositivos(self, modelo, numero_serie, estado):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "INSERT INTO dispositivos (modelo, numero_serie, estado) VALUES (%s, %s, %s)"
+            values = (modelo, numero_serie, estado)
+            cursor.execute(query, values)
+            self.connection.commit()
+            cursor.close()
+            print("Datos insertados correctamente")
+        else:
+            print("No hay conexión a la base de datos")
+                    
+    def actualizar_dispositivo(self, modelo_nuevo, numero_serie_nuevo, estado_nuevo, id_dispositivo):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "UPDATE dispositivos SET modelo = %s, numero_serie = %s, estado = %s WHERE id = %s"
+            values = (modelo_nuevo, numero_serie_nuevo, estado_nuevo, id_dispositivo)
+            cursor.execute(query, values)
+            self.connection.commit()
+            cursor.close()
+            print("Dispositivo actualizado correctamente")
+        else:
+            print("No hay conexión a la base de datos")
+    
+    def leer_instalaciones(self):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "SELECT * FROM instalaciones"
+            cursor.execute(query)
+            instalaciones = cursor.fetchall()
+            cursor.close()
+            return instalaciones
+        else:
+            print("No hay conexión a la base de datos")
+
+    def ingresar_instalacion(self, dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "INSERT INTO instalaciones (dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas) VALUES (%s, %s, %s, %s)"
+            values = (dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas)
+            cursor.execute(query, values)
+            self.connection.commit()
+            cursor.close()
+            print("Instalación insertada correctamente")
+        else:
+            print("No hay conexión a la base de datos")
+                    
+    def actualizar_instalacion(self, dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "UPDATE instalaciones SET direccion_instalacion = %s, fecha_instalacion = %s, coordenadas = %s WHERE id = %s"
+            values = (dispositivo_id, direccion_instalacion, fecha_instalacion, coordenadas)
+            cursor.execute(query, values)
+            self.connection.commit()
+            cursor.close()
+            print("Instalación actualizada correctamente")
+        else:
+            print("No hay conexión a la base de datos")
+
