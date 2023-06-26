05b211940a7e20df9b6ca05d5cf54f8c6ce0f7e7
Luis Emanuel Gomez <129911435+emma22xt@users.noreply.github.com>
2023-06-26 19:42:11 -0300

    Add files via upload

--- Sofware/evidencia N°4/proyecto base de datos evidencia 4.sql
+++ Sofware/evidencia N°4/proyecto base de datos evidencia 4.sql
@@ -1,1 +1,13 @@
-SELECT * FROM proyecto_integrador_1.dispositivos;
+CREATE TABLE if not exists Dispositivos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    numero_serie VARCHAR(50) NOT NULL,
+    estado VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE if not exists Instalaciones (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    direccion_instalacion VARCHAR(100) NOT NULL,
+    fecha_instalacion DATE NOT NULL,
+    coordenadas VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id) REFERENCES Dispositivos(id)
