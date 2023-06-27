b1b2e47dae5895ec312892dd9e65007da1ec72a6
María Micaela Vilchez Cabrera <129962506+MicaelaVilchez@users.noreply.github.com>
2023-06-26 21:01:43 -0300

    Update proyectointegradorv01.sql
    Se corrige las claves primarias y foránea de la taba Instalaciones

--- Sofware/evidencia N°4/proyectointegradorv01.sql
+++ Sofware/evidencia N°4/proyectointegradorv01.sql
@@ -1,1 +1,16 @@
-use proyectointegradorv01
+use proyectointegradorv01;
+CREATE TABLE if not exists Dispositivos (
+    id_dispositivo INT AUTO_INCREMENT PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    numero_serie VARCHAR(50) NOT NULL,
+    estado VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE if not exists Instalaciones (
+    id_Instalacion INT AUTO_INCREMENT PRIMARY KEY,
+    id_dispositivo INT NOT NULL,
+    direccion_instalacion VARCHAR(100) NOT NULL,
+    fecha_instalacion DATE NOT NULL,
+    coordenadas VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_dispositivo) REFERENCES Dispositivos(id)
+);
